992d6e102add5ef21d6d4c1de87d34f8dd932067
Sipu Kumar <sipuk1896@gmail.com>
2026-01-29 01:28:15 +0530

    new

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,6 +1,14 @@
-const { Children } = require("react")
 
-const reactElement={
+function customRender(reactElement,container){
+    const domElement=document.createElement(reactElement.type)
+    domElement.innerHTML=reactElement.Children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+    container.appendChild(domElement)
+    // ye function reactElement ko leke aayega aur usko domElement me convert karke container me append kar dega
+    // ye rendering ka kaam karega
+}
+    const reactElement={
     type: 'a',
     props: {
         href: 'https://www.google.com',
