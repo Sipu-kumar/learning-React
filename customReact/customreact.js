9e4404f4d431717214a6528f15355b9789827a3e
Sipu Kumar <sipuk1896@gmail.com>
2026-01-29 01:06:48 +0530

    updated

---
+++ customReact/customreact.js
@@ -0,0 +1,12 @@
+const { Children } = require("react")
+
+const reactElement={
+    type: 'a',
+    props: {
+        href: 'https://www.google.com',
+        target: '_blank',
+    },
+    Children: 'Click me to viste google'
+}
+
+const mainCointainer=document.querySelector('#root')
